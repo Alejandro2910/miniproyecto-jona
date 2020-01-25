bc303552d34f1f5d680f1d0814d693c8fc6367ee
Alejandro2910 <bernyc2910@gmail.com>
2020-01-25 12:30:26 -0600

    Pushing second commit, ABCDEF equations

--- miniproyecto2-dinamica.py
+++ miniproyecto2-dinamica.py
@@ -2,20 +2,16 @@ import matplotlib
 import math
 import numpy
 
-masa_persona = 0
-materiales = 0
-masa_silla = 0
-duracion_movimiento = 0
+masa_persona = materiales = masa_silla = duracion_movimiento = 0
 lp = 0.65
-w2 = 0.0
+alpha2 = w2 = w4 = theta2 = theta3 = theta4 = 0.0
 #Probar módulo para reducir longitud
 longitud_eslabones = numpy.array([1.14, 1.28, 1.14, 1.28])
 masa_eslabones = numpy.zeros((1, 4))
 peso_eslabones = numpy.zeros((1, 4))
 momento_inercia_eslabones = numpy.zeros((1, 4))
-#Por definir
-ancho = 0
-espesor = 0
+ancho = 0.05
+espesor = 0.02
 
 def solicitud_datos_entrada():
     global masa_persona, materiales, masa_silla, duracion_movimiento
@@ -23,16 +19,16 @@ def solicitud_datos_entrada():
     while entrada > 120.0:
         entrada = input("Favor ingresar la masa de la persona, debe ser menor a 120kg")
         if entrada <= 120.0:
-            #Caych float error
+            #Catch float error
             masa_persona = float(entrada)
         else:
             print("El valor está fuera de los límites, favor ingresarlo de nuevo.")
 
     entrada = 0
     #Revisar materiales
-    materiales = input("Seleccione el numero con los materiales que tendrá el mecanismo\n1. Aluminio\n2. Acero\n3. Titaneo")
+    materiales = input("Seleccione el numero con los materiales que tendrá el mecanismo\n1. Acero inoxidable\n2. Acero A36\n3. Fibra de carbono(HEXCEL AS4C)\n4. Aleación de aluminio(5052-H38 BARRA (SS))\n5. Aleación de aliminio(5086-H32 BARRA (SS))")
     
-    entrada = input("Seleccione la masa de la silla\n1. 14.5kg\n2. 17kg\n3. 24kg")
+    entrada = input("Seleccione la masa de la silla\n1. 17kg\n3. 24kg")
 
     if entrada == 1:
         masa_silla = 14.5
@@ -53,26 +49,32 @@ def solicitud_datos_entrada():
         duracion_movimiento = 190
 
 def calcule_w2(duracion_movimiento):
-    global w2
+    global w2, w4
     w2 = (2*math.pi)/duracion_movimiento
+    w4 = w2
 
 def calcule_masa_eslabones(materiales):
     global masa_eslabones
     densidad = 0
-    
+    volumen = 0.01
+
     if materiales == 1:
-        densidad = 2700
+        densidad = 7860
     elif materiales == 2:
         densidad = 7850
-    else:
-        densidad = 4507
+    elif materiales == 3:
+        densidad = 1780
+    elif materiales  == 4:
+        densidad = 2680
+    elif materiales == 5:
+        densidad = 2660
 
     for i in range(4):
         if i != 1:
             masa_eslabones[i] = densidad * ancho * espesor * longitud_eslabones[i]
         else:
             pass
-            #masa_eslabones[i] = densidad * volumen
+            masa_eslabones[i] = densidad * volumen
 
 def calcule_peso_eslabones(masa_eslabones):
     global peso_eslabones
@@ -89,4 +91,23 @@ def calcule_momento_inercia(masa_eslabones, longitud_eslabones):
             momento_inercia_eslabones[i] = (1/12)* masa_eslabones[i] * math.pow(longitud_eslabones[i], 2)
         else:
             pass
-            #Es fija 
+            momento_inercia_eslabones[i] = 1.24
+
+def calcule_A(theta4, longitud_eslabones):
+    return longitud_eslabones[2]*math.sin(theta4)
+
+def calcule_B(theta3, longitud_eslabones):
+    return longitud_eslabones[1]*math.sin(theta3)
+
+def calcule_C(alpha2, w2, w3, w4, theta2, theta3, theta4, longitud_eslabones):
+    return (longitud_eslabones[0]*alpha2*math.sin(theta2)) + (longitud_eslabones[0]*math.pow(w2, 2)*math.cos(theta2)) + (longitud_eslabones[1]*math.pow(w3, 2)*math.cos(theta3)) - (longitud_eslabones[2]*math.pow(w4, 2)*math.cos(theta4))
+
+def calcule_D(theta4, longitud_eslabones):
+    return longitud_eslabones[2]*math.cos(theta4)
+
+def calcule_E(theta3, longitud_eslabones):
+    return longitud_eslabones[1]*math.cos(theta3)
+
+def calcule_F(alpha2, w2, w3, w4, theta2, theta3, theta4, longitud_eslabones):
+    return (longitud_eslabones[0]*alpha2*math.cos(theta2)) - (longitud_eslabones[0]*math.pow(w2, 2)*math.sin(theta2)) - (longitud_eslabones[1]*math.pow(w3, 2)*math.sin(theta3)) + (longitud_eslabones[2]*math.pow(w4, 2)*math.sin(theta4))
+
